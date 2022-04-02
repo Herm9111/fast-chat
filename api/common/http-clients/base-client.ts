de0436be53f0e18612a34471c283f587b5d05c53
Mau Herrera <herm9111@gmail.com>
2022-04-02 12:38:50 -0700

    Fetcing connection string

---
+++ api/common/http-clients/base-client.ts
@@ -0,0 +1,45 @@
+import { HttpRequestHeaders } from "@azure/functions";
+const https = require('https')
+
+export async function get<T>(hostname:string, path: string, headers: HttpRequestHeaders): Promise<T> {
+    return new Promise((resolve, reject) => {
+        const options = {
+            hostname,
+            port:443,
+            path,
+            method:'GET',
+            headers
+        }
+
+        const req = https.request(options, res => {
+            const statusCode = res.statusCode;
+            let error;
+
+            if (statusCode !== 200) {
+                error = new Error('Request Failed.\n' +
+                    `Status Code: ${statusCode}`);
+            }
+
+            if (error) {
+                console.log(error.message);
+                reject(error.message);
+                // consume response data to free up memory
+                res.resume();
+                return;
+            }
+
+
+            res.on('data', (d) => {
+                process.stdout.write(d)
+
+                resolve(d)});
+        })
+
+       req.on('error', (e) => {
+            reject(`Got error: ${e.message}`);
+        });
+
+        req.end();
+    });
+
+}
--- api/getPubSubConnection/index.ts
+++ api/getPubSubConnection/index.ts
@@ -1,10 +1,19 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { get } from "../common/http-clients/base-client";
+
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+    const header = req.headers['x-ms-client-principal'];
+    const encoded = Buffer.from(header, 'base64');
+    const decoded = JSON.parse(encoded.toString('ascii'));
+    let downheaders = {}
+    downheaders['x-ms-client-principal-name'] = decoded.userDetails;
+    const dataResponse = await get<any>('fast-chat-api.azurewebsites.net', '/api/getpubsubconnection', downheaders);
     context.res = {
         status: 200, /* Defaults to 200 */
-        body: "DummyConnection"
+        body: dataResponse
     };
+
 };
 
 export default httpTrigger;
